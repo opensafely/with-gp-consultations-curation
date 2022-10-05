b9898c439820a689fa2ca3781dc4c20ea8aa1d04
Jon Massey <jon.massey@thedatalab.org>
2022-10-05 14:36:23 +0100

    pivot!

--- analysis/summarise.py
+++ analysis/summarise.py
@@ -2,13 +2,18 @@ from pathlib import Path
 from glob import glob
 import pandas as pd
 
-# totalise measure tables
+# pivot measure tables
 for f in glob("output/measure_gp_consultations_by_*[a-z]?.csv"):
     demographic = Path(f).stem.replace("measure_gp_consultations_by_", "")
     df = pd.read_csv(f)
-    f_out = Path(f.replace(demographic, f"{demographic}_total"))
+    f_out = Path(f.replace(demographic, f"{demographic}_pivot"))
     f_out = Path(f_out.parts[0], "tables", f_out.parts[1])
-    df.groupby(demographic)[["gp_consultation_count", "population"]].sum().to_csv(f_out)
+    df.pivot_table(
+        index=demographic,
+        values=["gp_consultation_count", "population"],
+        columns="date",
+        margins=True,
+    ).to_csv(f_out)
 
 # overall summary statistics
 df = pd.read_csv("output/measure_gp_consultations.csv")
@@ -17,6 +22,7 @@ dfg = df.groupby("date")[["gp_consultation_count", "population"]].agg(
     ["sum", "describe"]
 )
 
+# flatten multindex
 dfg.columns = [
     f"{c[0]}_sum"
     if c[1] in ["gp_consultation_count", "population"]
